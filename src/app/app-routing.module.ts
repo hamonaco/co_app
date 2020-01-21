5d4ef57f7f017ad72dc7bf777a6c5515de7070b5
Martin Gabriel Canone <49507846+kano37@users.noreply.github.com>
2020-01-21 09:00:30 -0300

    creacion de paginas de vistas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,27 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)},
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'familia',
+    loadChildren: () => import('./pages/familia/familia.module').then( m => m.FamiliaPageModule)
+  },
+  {
+    path: 'producto',
+    loadChildren: () => import('./pages/producto/producto.module').then( m => m.ProductoPageModule)
+  },
+  {
+    path: 'nueva-oferta',
+    loadChildren: () => import('./pages/nueva-oferta/nueva-oferta.module').then( m => m.NuevaOfertaPageModule)
+  },
 ];
 
 @NgModule({
