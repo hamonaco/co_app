d41892de44112ef439f7b374d8c44fca81fc7b0b
Martin Gabriel Canone <49507846+kano37@users.noreply.github.com>
2020-03-24 15:57:58 -0300

    nueva oferta arreglos

--- src/app/pages/nueva-oferta/nueva-oferta.page.ts
+++ src/app/pages/nueva-oferta/nueva-oferta.page.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nueva-oferta.page.scss'],
 })
 export class NuevaOfertaPage implements OnInit {
+  customPickerOptions;
+  customDate: Date = new Date();
 
   constructor() { }
 
   ngOnInit() {
+    this.customPickerOptions = {
+      buttons: [{
+        text: 'Ok',
+        handler: (data) => {
+          console.log('Clicked Ok');
+          console.log(data);
+          this.customDate = data.year.text;
+          console.log(this.customDate);
+        }
+      }, {
+        text: 'Cancelar',
+        handler: () => {
+          console.log('Clicked Log. Do not Dismiss.');
+        }
+      }]
+    };
   }
 
 }
